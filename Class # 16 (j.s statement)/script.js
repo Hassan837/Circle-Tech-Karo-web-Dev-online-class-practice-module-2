f00b6d38d3cd7fc3c1858038fda8ffe18697b63c
Hassan <hassanirfan429@gmail.com>
2021-05-19 23:02:31 +0500

    js query selector with using combinators done

--- Class # 16 (j.s statement)/script.js
+++ Class # 16 (j.s statement)/script.js
@@ -11,5 +11,15 @@ function login() {
 
     headingValue.innerHTML = inputValue;
 
-    
+    let inputElement2 = document.querySelector("#LN");
+    console.log(inputElement2);
+
+    let inputValue2 = inputElement2.value;
+    console.log (inputValue2);
+
+    let headingValue2 = document.querySelector('h1 > span.sp');
+    console.log (headingValue2);
+
+    headingValue2.innerHTML = inputValue2;
+
 }
