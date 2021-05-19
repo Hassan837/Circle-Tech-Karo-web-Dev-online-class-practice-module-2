3f5996d4e9e58a1fb40ce58dcf007f229c7ca514
Hassan <hassanirfan429@gmail.com>
2021-05-19 23:49:11 +0500

    js query variable practice

---
+++ Class # 17 (J.S Variable's)/script.js
@@ -0,0 +1,13 @@
+function show(){
+   const inputElement = document.querySelector('input').value;
+    console.log(inputElement);
+    console.log(inputElement * 2);
+    console.log(inputElement * 3);
+    console.log(inputElement * 4);
+    console.log(inputElement * 5);
+    console.log(inputElement * 6);
+    console.log(inputElement * 7);
+    console.log(inputElement * 8);
+    console.log(inputElement * 9);
+    console.log(inputElement * 10);
+}
