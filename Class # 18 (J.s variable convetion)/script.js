ae50daa3abfb7c84f4ef189a7f4d4481eb29d038
Hassan <hassanirfan429@gmail.com>
2021-05-23 11:24:25 +0500

    convert int to string

---
+++ Class # 18 (J.s variable convetion)/script.js
@@ -0,0 +1,15 @@
+//convert number into string
+
+let my_num = 4;
+
+let my_num2 = my_num.toString();
+
+
+//
+
+let myString = "2.3";
+
+let a = parseInt(myString);
+
+let b = parseFloat(myString);
+
