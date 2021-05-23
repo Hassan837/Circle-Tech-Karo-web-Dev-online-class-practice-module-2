a6b09c49502e10292e9dee277ee54f36cea04fd2
Hassan <hassanirfan429@gmail.com>
2021-05-23 11:06:28 +0500

    conversion

---
+++ Class # 18 (J.s variable convetion)/type-conversion.js
@@ -0,0 +1,8 @@
+// type converstion
+
+function check(){
+    const costOfApples = parseFloat (document.querySelector('input').value);
+    
+    alert (costOfApples);
+    console.log(typeof costOfApples);
+}
