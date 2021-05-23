6700bc21051fab45c94539cd961e6e9e5367fdda
Hassan <hassanirfan429@gmail.com>
2021-05-23 14:00:00 +0500

    online class task

---
+++ Class # 20 (node.js app)/traffic light problem/script.js
@@ -0,0 +1,23 @@
+/*
+        function printVariable(name){
+            let name1 = name;
+            console.log(name1);
+        }*/
+
+
+        function printVariable(){
+        
+        let a = 'hassan';
+
+        if(a == 'abc'){
+            alert('hassan not equal to abc');
+        }
+
+        else
+        {alert('Name is not match');
+    }
+        }
+
+
+        
+        
