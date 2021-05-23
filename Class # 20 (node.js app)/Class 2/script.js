8d201b6e44cc36401dfedd54d6765bda6bed4e05
Hassan <hassanirfan429@gmail.com>
2021-05-23 13:33:05 +0500

    practice if else

--- Class # 20 (node.js app)/Class 2/script.js
+++ Class # 20 (node.js app)/Class 2/script.js
@@ -1,6 +1,17 @@
-
+/*
         function printVariable(name){
             let name1 = name;
             console.log(name1);
+        }*/
+        function printVariable(){
+        
+        let a = 'hassan';
+
+        if(a == 'hassan'){
+            alert('hassan not equal to abc');
         }
+        }
+
+
+        
         
