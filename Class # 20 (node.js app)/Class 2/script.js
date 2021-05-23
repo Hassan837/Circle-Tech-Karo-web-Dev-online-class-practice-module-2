23edda45e13f4c013d7fd1f03a10978a8ba537ce
Hassan <hassanirfan429@gmail.com>
2021-05-23 13:54:21 +0500

    function practice js

--- Class # 20 (node.js app)/Class 2/script.js
+++ Class # 20 (node.js app)/Class 2/script.js
@@ -3,13 +3,19 @@
             let name1 = name;
             console.log(name1);
         }*/
+
+
         function printVariable(){
         
         let a = 'hassan';
 
-        if(a == 'hassan'){
+        if(a == 'abc'){
             alert('hassan not equal to abc');
         }
+
+        else
+        {alert('Name is not match');
+    }
         }
 
 
