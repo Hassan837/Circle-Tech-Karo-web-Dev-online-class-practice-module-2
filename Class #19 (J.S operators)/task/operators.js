e6bf1d137749a54d96df7affcea9e8dc41389941
Hassan <hassanirfan429@gmail.com>
2021-05-25 04:16:39 +0500

    JS operator task variable display & adding number done

--- Class #19 (J.S operators)/task/operators.js
+++ Class #19 (J.S operators)/task/operators.js
@@ -1,7 +1,20 @@
+//task # 1
+function create(){
+    let a = 4 , b = 12 , c;
+
+    c = a + b;
+
+    alert('The Sum 0f'+' ' + a + '+' + b + '=' + c);
+}
+
+
+
+// task # 2
 let score = 0;
+document.write('The value of varible is '+ score);
 
 function show(){
-    score += 4;
+    score += 5;
 
     document.querySelector('p').innerHTML = score
 }
