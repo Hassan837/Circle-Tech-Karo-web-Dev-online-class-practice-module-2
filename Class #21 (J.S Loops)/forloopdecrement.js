8fdc6330549d896a839d31638bf722e8bf37a78d
Hassan <hassanirfan429@gmail.com>
2021-05-27 03:01:06 +0500

    JS for loop increment & decrement

---
+++ Class #21 (J.S Loops)/forloopdecrement.js
@@ -0,0 +1,9 @@
+/* For  loop
+
+count from 270 to 200
+
+*/
+
+for(let i = 270; i>=200; i--){
+    console.log(i);
+}
---
+++ Class #21 (J.S Loops)/forloopincrement.js
@@ -0,0 +1,17 @@
+/* For  loop
+
+count from 200 to 270
+
+*/
+
+for(let i = 200; i<=270; i++){
+    console.log(i);
+}
+
+for(let i = 0; i<=10; i = i + 1){
+    console.log(i);
+}
+
+for(let i = 0; i<=20; i+=2){
+    console.log(i);
+}
