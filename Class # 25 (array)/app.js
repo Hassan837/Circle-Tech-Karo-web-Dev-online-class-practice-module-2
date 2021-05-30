525dccbd455750ff39125ed78d7e0f121ecab4fa
Hassan <hassanirfan429@gmail.com>
2021-05-30 15:03:14 +0500

    array with loop practice

--- Class # 25 (array)/app.js
+++ Class # 25 (array)/app.js
@@ -1,14 +1,36 @@
 let games = ["cricket","Tennis","Vollyball", "Squash"];
 //let game = "cricket";
 
-for (let i=0; i < games.length; i++){
+//adding array in last
+games.push("Table tennis");
+games.push("Basketball");
+console.log(games);
+
+// adding array in first
+games.unshift("Polo");
+console.log(games);
+
+// remove the last element of array
+games.pop();
+console.log(games);
+
+// remove the first element  of array
+games.shift();
+console.log(games);
+
+// remove the element from center
+games.splice(2, 0 , "UNO" , "LUDO");
+console.log(games);
+
+
+/*for (let i=0; i < games.length; i++){
 
     if (games[i] == "cricket"){
         alert("The Cricket is Exist");
     }
 
     else {"not match any thing"};
-}
+}*/
 
 /*let gamesObject = {
     first: "cricket",
