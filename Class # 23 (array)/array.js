ae5330d177defad5c53df197dd60e8d2006a6225
Hassan <hassanirfan429@gmail.com>
2021-05-30 14:26:02 +0500

    array with loop practice

---
+++ Class # 23 (array)/array.js
@@ -0,0 +1,11 @@
+let games = ["cricket","Tennis","Vollyball", "Squash"];
+let game = "cricket";
+
+let gamesObject = {
+    first: "cricket",
+    second: "Tennis",
+    Third: "Vollyball",
+    Fourth: "Squash"
+}
+
+console.log(gamesObject);
---
+++ Class # 25 (array)/app.js
@@ -0,0 +1,20 @@
+let games = ["cricket","Tennis","Vollyball", "Squash"];
+//let game = "cricket";
+
+for (let i=0; i < games.length; i++){
+
+    if (games[i] == "cricket"){
+        alert("The Cricket is Exist");
+    }
+
+    else {"not match any thing"};
+}
+
+/*let gamesObject = {
+    first: "cricket",
+    second: "Tennis",
+    Third: "Vollyball",
+    Fourth: "Squash"
+}
+
+console.log(gamesObject); */
