f696ed746de7dd48e9d7e71b1ba05f48ad9cc4d4
Hassan <hassanirfan429@gmail.com>
2021-06-06 13:22:54 +0500

    adding button

--- Class #29 (DOM CSS)/app.js
+++ Class #29 (DOM CSS)/app.js
@@ -1,4 +1,17 @@
+function showStyl(){
+
 document.querySelector('div').style.backgroundColor = "lightgreen"
 let element = document.querySelector('div').style.border;
 
 console.log(element);
+
+document.querySelector('#heading').style.backgroundColor = "lightblue"
+let element1 = document.querySelector('#heading').style.border;
+
+console.log(element1);
+
+document.querySelector('.head1').style.backgroundColor = "lightgreen"
+let element2 = document.querySelector('.head1').style.border;
+
+console.log(element2);
+}
