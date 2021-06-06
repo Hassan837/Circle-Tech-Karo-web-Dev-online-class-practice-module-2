0c1014457b1de1859598bf63aacd04e06afe5f23
Hassan <hassanirfan429@gmail.com>
2021-06-06 14:05:19 +0500

    changing class name by js

--- Class #29 (DOM CSS)/app.js
+++ Class #29 (DOM CSS)/app.js
@@ -1,35 +1,38 @@
-function showStyle(){
+function showStyle() {
 
-document.querySelector('div').style.backgroundColor = "lightgreen"
-let element = document.querySelector('div').style.border;
+    document.querySelector('div').style.backgroundColor = "lightgreen"
+    let element = document.querySelector('div').style.border;
 
-console.log(element);
+    console.log(element);
 
-document.querySelector('#heading').style.backgroundColor = "lightblue"
-let element1 = document.querySelector('#heading').style.border;
+    document.querySelector('#heading').style.backgroundColor = "lightblue"
+    let element1 = document.querySelector('#heading').style.border;
 
-console.log(element1);
+    console.log(element1);
 
-document.querySelector('.head1').style.backgroundColor = "lightgreen"
-let element2 = document.querySelector('.head1').style.border;
+    document.querySelector('.head1').style.backgroundColor = "lightgreen"
+    let element2 = document.querySelector('.head1').style.border;
 
-console.log(element2);
+    console.log(element2);
 }
 
-function showStyle1(){
+function showStyle1() {
 
     document.querySelector('div').style.backgroundColor = "pink"
     let element = document.querySelector('div').style.border = "3px dotted darkGreen";
-    
+
     console.log(element);
-    
+
     document.querySelector('#heading').style.backgroundColor = "purple"
     let element1 = document.querySelector('#heading').style.border = "3px dotted darkblue";
-    
+
     console.log(element1);
-    
+
     document.querySelector('.head1').style.backgroundColor = "blue"
     let element2 = document.querySelector('.head1').style.border = "3px dotted red";
-    
+    let element3 = document.querySelector('.head1').className;
+    let element4 = document.querySelector('.head1').className = "headingnName"; 
     console.log(element2);
-    }
+    console.log(element3);
+    console.log(element4);
+}
