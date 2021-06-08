2802809456dc05cb681f7471610bda83025c5762
Hassan <hassanirfan429@gmail.com>
2021-06-08 07:05:14 +0500

    array each loop

--- Class #24 (More about Arrays and Sorting in JS)/app.js
+++ Class #24 (More about Arrays and Sorting in JS)/app.js
@@ -10,3 +10,18 @@ function displayNames(){
         console.log("Student #" + (i+1) + "=>" + names[i]);
     }
 }
+
+let scores = [34 , 45 , 3 , 76 , 93];
+
+//for each loop
+
+function calcuAvg(){
+let sum = 0;
+for ( let i=0;i < scores.length; i++){
+    sum = sum + scores[i]
+}
+let average = sum / scores.length;
+
+console.log("average", average);
+console.log("sum", sum)
+}
